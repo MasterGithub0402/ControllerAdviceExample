a608be6c073c326f794c28ee07606ee6ea7787c3
MasterGithub0402 <mastergithub0402@gmail.com>
2023-04-30 20:02:56 +0530

    Made some changes

--- CodeDecodeCRUD/src/main/java/com/codedecode/demo/controller/HomeController.java
+++ CodeDecodeCRUD/src/main/java/com/codedecode/demo/controller/HomeController.java
@@ -5,10 +5,7 @@ import com.codedecode.demo.repos.EmployeeCrudRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
@@ -17,8 +14,14 @@ public class HomeController {
     EmployeeCrudRepo empRepo;
 
     @GetMapping("/get/{empid}")
-    public ResponseEntity<Employee> saveDate(@PathVariable("empid") Long id) {
+    public ResponseEntity<Employee> getDate(@PathVariable("empid") Long id) {
         Employee employee = empRepo.findById(id).get();
         return new ResponseEntity<>( employee, HttpStatus.OK);
+    }
+
+    @PostMapping("save2")
+    public ResponseEntity<Employee> saveDate(@RequestBody Employee employee) {
+        Employee employee1 = empRepo.save(employee);
+        return new ResponseEntity<>(employee1, HttpStatus.CREATED);
     }
 }
